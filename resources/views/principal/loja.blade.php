f6cdaf535133a24d471340e25be9599e8dcd2d90
ArtieMalakianT <maiconalexdesouza@gmail.com>
2019-06-27 15:23:48 -0300

    Mudança no layout de produtos

--- resources/views/principal/loja.blade.php
+++ resources/views/principal/loja.blade.php
@@ -49,11 +49,11 @@
             <a class="product-mask" href="#">                                                          
                 <div class="img-product-back"> 
                         <img class="tag" src="img/Etiqueta_novo.png">                    
-                        <img class="img-product-secundary" src="img/products/lingerie2.jpg" alt="langerie1">                            
+                        <img class="img-product-secundary" src="img/products/langerie2.jpg" alt="langerie1">                            
                 </div>
                 <div class="img-product-front">
                         <img class="tag" src="img/Etiqueta_novo.png">                                                                    
-                        <img class="img-product" src="img/products/langerie1.png" alt="langerie1">                                
+                        <img class="img-product" src="img/products/langerie1.jpg" alt="langerie1">                                
                 </div>
             </a>                        
         </div>
@@ -68,12 +68,15 @@
     <!-- Produto 2 -->
     <li class="product">                          
         <div class="product-top">                        
-            <a href="#">
-                <figure>
-                    <img class="tag" src="img/Etiqueta_novo.png">                  
-                    <img class="img-product" src="img/products/lingerie2.jpg" alt="langerie1">
-                    <figcaption></figcaption> 
-                </figure>
+            <a class="product-mask" href="#">                                                          
+                <div class="img-product-back"> 
+                        <img class="tag" src="img/Etiqueta_novo.png">                    
+                        <img class="img-product-secundary" src="img/products/langerie1.jpg" alt="langerie1">                            
+                </div>
+                <div class="img-product-front">
+                        <img class="tag" src="img/Etiqueta_novo.png">                                                                    
+                        <img class="img-product" src="img/products/langerie2.jpg" alt="langerie1">                                
+                </div>
             </a>                            
         </div>
     
@@ -87,13 +90,16 @@
     <!-- Produto 3 -->
     <li class="product">                          
             <div class="product-top">                        
-                <a href="#">
-                    <figure>
-                        <img class="tag" src="img/Etiqueta_novo.png">                  
-                        <img class="img-product" src="img/products/lingerie3.jpg" alt="langerie1">
-                        <figcaption></figcaption> 
-                    </figure>
-                </a>
+            <a class="product-mask" href="#">                                                          
+                <div class="img-product-back"> 
+                        <img class="tag" src="img/Etiqueta_novo.png">                    
+                        <img class="img-product-secundary" src="img/products/langerie3.jpg" alt="langerie1">                            
+                </div>
+                <div class="img-product-front">
+                        <img class="tag" src="img/Etiqueta_novo.png">                                                                    
+                        <img class="img-product" src="img/products/langerie4.jpg" alt="langerie1">                                
+                </div>
+            </a>
             </div>
     
             <div class="product-footer">
@@ -106,13 +112,16 @@
     <!-- Produto 4 -->
     <li class="product">                          
             <div class="product-top">                        
-                <a href="#">
-                    <figure>
-                        <img class="tag" src="img/Etiqueta_novo.png">                  
-                        <img class="img-product" src="img/products/lingerie4.jpg" alt="langerie1">
-                        <figcaption></figcaption> 
-                    </figure>
-                </a>
+            <a class="product-mask" href="#">                                                          
+                <div class="img-product-back"> 
+                        <img class="tag" src="img/Etiqueta_novo.png">                    
+                        <img class="img-product-secundary" src="img/products/langerie4.jpg" alt="langerie1">                            
+                </div>
+                <div class="img-product-front">
+                        <img class="tag" src="img/Etiqueta_novo.png">                                                                    
+                        <img class="img-product" src="img/products/langerie3.jpg" alt="langerie1">                                
+                </div>
+            </a>
             </div>
     
             <div class="product-footer">
